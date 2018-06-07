0928d61823c9e4e46a4ec27cc12f9afad4e9b0ac
Tyler Shellberg <tyler.shellberg@student.nmt.edu>
2018-06-06 19:35:59 -0600

    Added JSON configuration for % to which guts can negate injury penalties.

--- InjuriesHurt.cs
+++ InjuriesHurt.cs
@@ -1,17 +1,35 @@
 ﻿using BattleTech;
 using Harmony;
+using Newtonsoft.Json;
+using System;
 using System.Reflection;
 
 namespace com.Tyler799.Battletech.InjuriesHurtMod
 {
     public class InjuriesHurt
     {
-        public static void Init()
+        public static Settings ModSettings;
+
+        public static void Init(string _directory, string settingsJSON)
         {
+            try
+            {
+                ModSettings = JsonConvert.DeserializeObject<Settings>(settingsJSON);
+            }
+            catch (Exception ex)
+            {
+                ModSettings = new Settings();
+            }
+
             // make Harmony call your hooks
             var harmony = HarmonyInstance.Create("com.Tyler799.Battletech.InjuriesHurtMod");
             harmony.PatchAll(Assembly.GetExecutingAssembly());
         }
+    }
+
+    public class Settings
+    {
+        public double gutsEffectPercentage = 0.5;
     }
 
     // Modifies tactics value
@@ -21,18 +39,7 @@ namespace com.Tyler799.Battletech.InjuriesHurtMod
     {
         public static void Postfix(Pilot __instance, ref int __result)
         {
-            __result = (int)(__result * (((0.05 * __instance.Guts - 1) * __instance.Injuries) / (float)__instance.Health + 1));
-        }
-    }
-
-    // Modifies gunnery value
-    [HarmonyPatch(typeof(Pilot))]
-    [HarmonyPatch("Gunnery", PropertyMethod.Getter)]
-    public class GunneryHealthModifier
-    {
-        public static void Postfix(Pilot __instance, ref int __result)
-        {
-            __result = (int)(__result * (((0.05 * __instance.Guts - 1) * __instance.Injuries) / (float)__instance.Health + 1));
+            __result = (int)(__result * ((((InjuriesHurt.ModSettings.gutsEffectPercentage / 10) * __instance.Guts - 1) * __instance.Injuries) / (float)__instance.Health + 1));
         }
     }
 
@@ -43,7 +50,18 @@ namespace com.Tyler799.Battletech.InjuriesHurtMod
     {
         public static void Postfix(Pilot __instance, ref int __result)
         {
-            __result = (int)(__result * (((0.05 * __instance.Guts - 1) * __instance.Injuries) / (float)__instance.Health + 1));
+            __result = (int)(__result * ((((InjuriesHurt.ModSettings.gutsEffectPercentage / 10) * __instance.Guts - 1) * __instance.Injuries) / (float)__instance.Health + 1));
+        }
+    }
+
+    // Modifies gunnery value
+    [HarmonyPatch(typeof(Pilot))]
+    [HarmonyPatch("Gunnery", PropertyMethod.Getter)]
+    public class GunneryHealthModifier
+    {
+        public static void Postfix(Pilot __instance, ref int __result)
+        {
+            __result = (int)(__result * ((((InjuriesHurt.ModSettings.gutsEffectPercentage / 10) * __instance.Guts - 1) * __instance.Injuries) / (float)__instance.Health + 1));
         }
     }
 }
